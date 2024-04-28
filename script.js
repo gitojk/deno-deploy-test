a71e082f923405aaaad9430e6e15ba75345310ac
gitojk <166489117+gitojk@users.noreply.github.com>
2024-04-28 20:20:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 async function getResource() {
-  const res = await fetch('http://localhost:8000');
+  const res = await fetch('https://gitojk-deno-deploy.deno.dev/');
   const obj = await res.json();
   console.log(obj);
   document.querySelector('h1').textContent = obj.env;
