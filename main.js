2e5baf4c4bcb566a53852ad0b0dd5ed05281f3b8
ojklab <26958104+ojklab@users.noreply.github.com>
2024-04-28 18:57:52 +0900

    下準備

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+import 'https://deno.land/std/dotenv/load.ts';
 
+Deno.serve(async (req) => {
+  // 環境変数の読み込み
+  const apiKey = Deno.env.get('API_KEY');
+  console.log(apiKey);
+
+  // メッセージボディを構成
+  const body = JSON.stringify({ env: apiKey });
+
+  return new Response(body, {
+    headers: {
+      'Access-Control-Allow-Origin': '*', // ここはあとできちんと設定する
+      'content-type': 'application/json; charset=utf-8',
+    },
+  });
+});
